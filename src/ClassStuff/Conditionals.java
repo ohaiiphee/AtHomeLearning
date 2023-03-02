8bf95e453f909544ecbf8ca4becc34d2b445a449
s53032 <sofia.martinsrua@gmail.com>
2023-03-02 14:42:16 +0100

    Look through class powerpoint 2 part2

---
+++ src/ClassStuff/Conditionals.java
@@ -0,0 +1,23 @@
+package ClassStuff;
+
+public class Conditionals {
+    public static void main(String[] args) {
+        /* Methods are blocks of code that perform a specific task
+           they can be called from other parts of the program
+           they typically take some input, process it, and return a result
+           they have an access modifier (public, protected, private)
+        */
+        /* Every method has:
+           a data type declared in method signature
+           a name
+           none or several parameters
+           a return value: return value „void“ if nothing is returned
+
+           -->     int name (int parameter1, float parameter3) {
+                   ...
+                   return returnValue;
+        */
+
+
+    }
+}
