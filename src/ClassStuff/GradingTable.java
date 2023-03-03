66a1edb8cdd9769745444979c6035e5dd84efa7f
s53032 <sofia.martinsrua@gmail.com>
2023-03-03 10:48:06 +0100

    Look through class powerpoint 2 part5

--- src/ClassStuff/GradingTable.java
+++ src/ClassStuff/GradingTable.java
@@ -1,26 +1,32 @@
 package ClassStuff;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class GradingTable {
     public static void main(String[] args) {
         int points;
 
-        getGrade(50);
-        getGrade(80);
-        getGrade(99);
+getGrade(49);
+getGrade(55);
+getGrade(78);
+getGrade(99);
+getGrade(2);
 
     }
 
     public static String getGrade(int points) {
         String fail = "Fail";
- if (points >= 90) {
-     System.out.println("Outstanding");
- } else if ( points > 76) {
-     System.out.println("Excellent");
-        } else if (points > 64) {
-     System.out.println("Acceptable");
- } else if (points > 50) {
-     System.out.println("Passing");
- }
- return fail;
+        if (points >= 90) {
+            System.out.println("Outstanding");
+        } else if (points >= 78) {
+            System.out.println("Excellent");
+        } else if (points >= 65) {
+            System.out.println("Acceptable");
+        } else if (points >= 51) {
+            System.out.println("Passing");
+        } else if (points <= 50){
+            System.out.println(fail);
+        } return fail;
     }
+
 }
