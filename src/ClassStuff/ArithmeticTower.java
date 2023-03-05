d0487dfef2a921df43dfcd3f7b3a237a9206df31
ohaiiphee <sofia.martinsrua@gmail.com>
2023-03-05 17:58:51 +0100

    arithmetic tower

---
+++ src/ClassStuff/ArithmeticTower.java
@@ -0,0 +1,35 @@
+package ClassStuff;
+
+public class ArithmeticTower {
+    public static void main(String[] args) {
+        int number = 1;
+        int numero = 9000000;
+
+        System.out.println(number);
+        multiply(number);
+
+
+        divide(numero);
+        System.out.println(divide(numero));
+
+    }
+
+    public static int multiply (int number) {
+        int result = number;
+        for (int i = 2; i <= 9; i++) {
+
+            System.out.println(result + "*" + i);
+            result = result * i;
+        }
+        return result;
+    }
+
+    public static int divide (int numero) {
+        int division = numero;
+        for (int i = 9; i >=2 ; i--) {
+            System.out.println(division + "/" + i);
+            division = division / i;
+        }
+        return division;
+    }
+}
